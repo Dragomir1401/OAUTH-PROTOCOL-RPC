e5e9263f8aacb5e4df6ba77b8871516eb1f7ddd8
Dragomir Andrei <andreidrago14@yahoo.com>
2024-11-05 23:50:11 +0200

    fixed types in server model

--- src/data/server/server_model.cpp
+++ src/data/server/server_model.cpp
@@ -7,7 +7,7 @@ ServerModel::ServerModel()
 
 ServerModel::ServerModel(
     int user_count,
-    std::unordered_map<std::string, access_token> token_to_access_token,
+    std::unordered_map<std::string, access_token_t> token_to_access_token,
     std::unordered_map<std::string, std::unordered_map<std::string, std::string>> token_to_token_permissions,
     std::unordered_map<std::string, std::unordered_map<std::string, std::string>> user_to_user_permissions,
     std::vector<std::string> user_list)
